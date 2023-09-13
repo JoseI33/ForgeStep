8f78eb8e7de2039f037ae85b43f376abafa61d52
Josei33 <jose.imhoff.20@gmail.com>
2023-09-13 00:05:03 -0300

    control

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = require('./app')
 const CONNECTDB = require('./src/config/bd');
 CONNECTDB();
 
-const port = 3010;
+const port = 3010; 
 
 app.listen (port, () => {
     console.log(`connection the port ${port}`);
