76501265b139c9e97b1087f472878422c05310c3
mauricio <maurimuoz@gmail.com>
2023-09-14 23:50:02 -0300

    se agregaron los archivos que no traia el pull (favorite)

---
+++ src/controller/favoriteController.js
@@ -0,0 +1,50 @@
+const FavModel = require("../models/favorite");
+
+const createFavorites = async (req, res) => {
+  const { user_id, product_id } = req.body;
+
+  const newFavorites = new FavModel({
+    user_id,
+    product_id,
+  });
+
+  try {
+    const favorite = await newFavorites.save();
+    return res.status(200).send(favorite);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ msg: "El producto ya existe en la base" });
+  }
+};
+
+const deleteFavorites = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await FavModel.findByIdAndDelete(id);
+    return res.status(200).send({ msg: "Product successfully deleted" });
+  } catch (error) {
+    res.status(400).send({ msg: "Product could not be removed" });
+  }
+};
+
+const getFavorites = async (req, res) => {
+  try {
+    const favorites = await FavModel.find({ user_id: req.params.id }).populate(
+      "product_id"
+    );
+    if (favorites) {
+      return res.status(200).send(favorites);
+    } else {
+      return res.status(200).send([]);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+};
+
+module.exports = {
+  createFavorites,
+  deleteFavorites,
+  getFavorites,
+};
