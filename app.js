ac17ff22582ad0fcfb8bbabb050c329dd7f1d912
José F. Imhoff <131836617+JoseI33@users.noreply.github.com>
2023-09-16 18:21:58 -0300

    Back (#12)
    * Installation of packages and creation of scaffolding
    
    * create the data base and archive
    
    * Uploading files
    
    * Creacion de modelos
    
    * Conexion con el controlador de usuario
    
    * control
    
    * revision en rama
    
    * Corrigiendo errores
    
    * Usercontroller, models user y route user funcionando
    
    * Creacion de controlador de autentificacion

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
+const usersRoute = require('./src/router/user.js');
+const authRoute = require('./src/router/auth.js')
 const products = require('./src/router/products');
 
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(express.json());
 
+app.use('/api/users', usersRoute);
+app.use('/api/auth', authRoute);
 app.use('/api/products', products);
 
-module.exports = app
+module.exports = app;
--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 require ('dotenv').config();
-const CONNECTDB = require('./src/Database/bd');
+const CONNECTDB = require('./src/config/bd');
 const app = require('./app');
-
 const port = 3010;
 
-
+const bodyparser = require('body-parser');
 CONNECTDB();
-
 
 app.listen (port, () => {
     console.log(`connection the port ${port}`);
